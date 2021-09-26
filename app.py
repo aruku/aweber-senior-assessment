0edf3c6d6e48a1590d934f03a43be674e096672e
Eduardo Thomas <eduardo.perezdelpostigo@bhnetwork.com>
2021-09-26 23:26:13 +0200

    Persist new widgets

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from tornado.web import Application, RequestHandler
 from tornado.ioloop import IOLoop
 import json
+import sqlite3
+import datetime
+
+# Initialize components
+conn = sqlite3.connect('.widgets.db')
+c = conn.cursor()
+
+try:
+    c.execute('''CREATE TABLE IF NOT EXISTS widgets (name TEXT, number_parts INT, created TEXT, updated TEXT)''')
+except sqlite3.OperationalError as e:
+    raise Exception("Can't create the table in the DB: " + str(e))
 
 widgets = []
 
@@ -10,7 +21,12 @@ class WidgetsHandler(RequestHandler):
 
 class WidgetHandler(RequestHandler):
     def post(self):
-        widgets.append(json.loads(self.request.body))
+        body_decoded = json.loads(self.request.body)
+        name = body_decoded["name"]
+        number_parts = body_decoded["number_parts"]
+        date_created = datetime.datetime.now()
+        c.execute("INSERT INTO widgets (name, number_parts, created) VALUES (?, ?, ?)", (name, number_parts, date_created))
+        conn.commit()
 
 def make_app():
     urls = [
